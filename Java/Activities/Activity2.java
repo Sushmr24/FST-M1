18c7e60f17ec00c54c37e02240faad26a4b718a4
Sushmr24 <97938738+Sushmr24@users.noreply.github.com>
2022-01-18 16:50:02 +0530

    Activities

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,24 @@
+package activities;
+
+public class Activity2 {
+    public static void main(String[] args) {
+        int[] data = {10, 77, 10, 54, -11, 10};
+        int count = 0;
+
+        for(int i = 0; i< data.length ; i++)
+        {
+            if (data[i] == 10)
+            {
+                count+=data[i] ;
+            }
+        }
+        if(count == 30)
+        {
+            System.out.println("Sum of 10's in the array is 30 ");
+        }
+        else
+        {
+            System.out.println("Sum of 10's in the array is either grater than or less than 30");
+        }
+   }
+}
